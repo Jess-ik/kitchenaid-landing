3ff432f9838ce26d40c1c85aa1a68619180a72ae
Jess <jessica.louvel@gmail.com>
2024-03-28 11:34:00 +0100

    Landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,14 @@
 import Image from "next/image";
 import Header from "./components/Header";
+import Landing from "./components/Landing";
 
 export default function Home() {
   return (
     <>
       <Header />
     <main className="h-screen">
-      
-      <h1 >Blue Salt</h1>
-      <h2 >Couleur KitchenAid 2024</h2>
-      <h3 >Découvrez notre fabuleux robot pâtissier</h3>
-      <p >Fouetter, batter, remuer, pétrir ou écrasier: 
-        ce superbe robot pâtissier Artisan de 4,7L peut tout faire.
-        Avec son mouvement planétaire unique et son design emblématique,
-        il incarne parfaitement la marque de robots pâtissiers nº1 dans le monde.
-      </p>
-      <button>Add to bag</button>
+    <Landing />
+     
       <h2 >Caractéristiques</h2>
     </main></>
   );
